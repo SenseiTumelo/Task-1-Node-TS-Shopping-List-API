ec8593c70f8c1d51206d8a7b5d37e170dc9fb9ab
SenseiTumelo <tmotjopi@gmail.com>
2026-09-08 08:52:24 +0200

    feat: created a server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import http, {IncomingMessage, ServerResponse } from 'http';
+import {shoppingListRoute} from './routes/shoppingList.js'
+
+
+const PORT = 3000; 
+
+const requestListener = (req: IncomingMessage, res: ServerResponse) =>{
+    if(req.url?.startsWith('/shoppingList')){
+        shoppingListRoute(req,res);
+    }
+    else {
+        res.writeHead(200, {"content-type":"application/json"});
+        res.end(JSON.stringify({message: "Hello World, the shopping Server is responding"}))
+    }
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
